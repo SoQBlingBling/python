cf414d86c3df36dd740baa472b4731ff8c756558
SoQBlingBling <1779742780@qq.com>
2023-07-19 00:02:48 +0800

    赋值运算

---
+++ test/1.字面量.py
@@ -0,0 +1,14 @@
+print('hello')
+print(123)
+print(1.23)
+print(1)
+# 单行注释
+ 
+'''
+nihao 
+'''
+
+
+# 检测变量类型
+print(type(1))
+ 
---
+++ test/2.数据类型.py
@@ -0,0 +1,6 @@
+#  通过type() 方法判断数据为什么类型
+
+
+print(type(123))
+print(type('hello'))
+print(type(11.2))
